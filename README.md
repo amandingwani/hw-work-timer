73c187c6b0cafad403e75b31d6de3eec34b2835a
Aman Dingwani <amandingwani@gmail.com>
2024-04-12 08:46:30 +0530

    feat: Add button to start/pause timer feat: Add led indicator for start/pause

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # hw-work-timer
-Firmware for a physical timer built with an Arduino and TM1637 display
+Countdown timer with LED display driven by TM1637 display module and Arduino.
+
+## TM1637 connections:
+CLK - Pin 4 of Arduino
+DIO - Pin 3 of Arduino
+VCC - 5V  of Arduino
+GND - GND  of Arduino
+
+# Led and Button connection:
+LED_PIN 5
+BUTTON_PIN 2
+
+## Libraries used
+
+### TM1637
+version=1.2.0
+author=Avishay Orpaz <avishorp@gmail.com>
+
+### CountDown
+version=0.3.3
+author=Rob Tillaart <rob.tillaart@gmail.com>
--- timer_v1.ino
+++ timer_v1.ino
@@ -4,11 +4,18 @@
 CountDown CD(CountDown::MINUTES);
 
 // Module connection pins (Digital Pins)
-#define CLK 2
+#define CLK 4
 #define DIO 3
+#define LED_PIN 5
+#define BUTTON_PIN 2
 
 // The amount of time (in milliseconds) between display updates
 #define REFRESH_DELAY  60000
+
+volatile byte ledState = LOW;
+
+long debouncing_time = 100; //Debouncing Time in Milliseconds
+volatile unsigned long last_micros;
 
 const uint8_t SEG_DONE[] = {
 	SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,           // d
@@ -25,15 +32,39 @@ int mmss(int mins) {
 }
 
 void setup() {
-  display.setBrightness(1);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), debounceInterrupt, RISING);
+  
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  display.setBrightness(0);
   display.showNumberDecEx(800, 0b01000000, true);
 
   // countdown start with minutes as arg
   CD.start(480);
+  CD.stop();
 }
 
 void loop() {
   display.showNumberDecEx(mmss(CD.remaining()), 0b01000000, true);
   
   delay(REFRESH_DELAY);
+}
+
+void handleButtonPress() {
+  ledState = !ledState;
+  if(ledState == LOW) {
+    CD.stop();
+  } else {
+    CD.start(CD.remaining());
+  }
+  digitalWrite(LED_PIN, ledState);
+}
+
+void debounceInterrupt() {
+  if((long)(micros() - last_micros) >= debouncing_time * 1000) {
+    last_micros = micros();
+    handleButtonPress();
+  }
 }
